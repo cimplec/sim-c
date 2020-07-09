4e49243cfc3db7988e14d2f4faa0bb41724bf5e4
dhairyaj <dhairya.jain.work@gmail.com>
2020-07-09 20:29:19 +0530

    Added datatype inside loop

--- compiler.py
+++ compiler.py
@@ -158,7 +158,7 @@ def compile(opcodes, c_filename, table):
         # If opcode is of type for
         elif opcode.type == "for":
             val = opcode.val.split('&&&')
-            code += "\tfor(" + val[0] + " = " + val[1] + " ; " + val[0] + " " + val[4] + " " + val[2] + " ; " + val[0] + val[3] + "=" + val[5] +"){\n"
+            code += "\tfor( int " + val[0] + " = " + val[1] + " ; " + val[0] + " " + val[4] + " " + val[2] + " ; " + val[0] + val[3] + "=" + val[5] +"){\n"
 
         outside_code, ccode = compile_func_main_code(outside_code, ccode, outside_main, code)
 
