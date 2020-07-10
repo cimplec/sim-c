be715e9cd4039700d0d23618f5e9180162352793
Aayush8899 <aayush.88@outlook.com>
2020-07-11 00:53:31 +0530

    added logical operators

--- test.c
+++ test.c
@@ -2,10 +2,11 @@
 
 int main() {
 	int a = 1;
-	if(a == 1) {
+	int b = 2;
+	if(a == 1 || b == 2) {
 	printf("One");
 	}
-	else if(a == 2) {
+	else if(a == 2 && b == 1) {
 	printf("Two");
 	}
 	else {
