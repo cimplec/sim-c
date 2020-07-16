cf42660fa660aa22bcfa5acd79d80716af82dad4
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-07-16 17:37:28 +0530

    Fix: comment strings

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ int sum(int a, int b) {
 
 	return a + b;
 }
-/* 
+/*
   Code written by:- Siddhartha
   Main starts
 */
@@ -13,14 +13,7 @@ int main() {
 	int hello = sum(1, 2);
 	printf("The sum = %d", hello);
 	exit(0);
-<<<<<<< HEAD
-	//  testing single line comment 
-=======
-	// 
-  Code written by:- Siddhartha
-  Main starts
- testing single line comment 
->>>>>>> 23d072e86c2c9f73fe6524dad29bfd314a8aec46
+	//  testing single line comment
 
 	return 0;
 }
