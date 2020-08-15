50b76770a7045550f7d357a9f0c8e6062368df2e
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-08-16 01:39:37 +0530

    Update simc testfile

--- test.c
+++ test.c
@@ -1,24 +1,9 @@
-#include <stdio.h>
 
-int a(int b) {
-
-	return 1;
-}
 
 int main() {
 	int a_var = 10;
 	int a = a_var << 1;
 	a = 1 >> a;
-	if(a < 4) {
-	a++;
-	}
-	while(a(1)) {
-	a_var = a_var - 1;
-	printf("yolo");
-	}
-	for(int i = 1; i < 10; i+=1) {
-	printf("%d", i);
-	}
 
 	return 0;
 }
