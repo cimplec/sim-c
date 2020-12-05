5aa62985e0b998a716c608d144cfc8324bb51f90
Mathias <mathfernandes@usp.br>
2020-12-05 16:06:55 -0300

    Start array_statment

--- simc/simc_parser.py
+++ simc/simc_parser.py
@@ -1162,6 +1162,129 @@ def var_statement(tokens, i, table, func_ret_type):
 
         return OpCode("var_no_assign", value), i + 1, func_ret_type
 
+def array_statement(tokens, i, table, func_ret_type):
+    """
+    Parse array declaration [/initialization] statement
+    Params
+    ======
+    tokens      (list) = List of tokens
+    i           (int)  = Current index in token
+    table       (SymbolTable) = Symbol table constructed holding information about identifiers and constants
+    func_ret_type (string) = Function return type
+    Returns
+    =======
+    OpCode, int: The opcode for the var_assign/var_no_assign code and the index after parsing var statement
+    Grammar
+    =======
+    var_statement   -> var id [= expr]?
+    expr            -> string | number | id | operator
+    string          -> quote [a-zA-Z0-9`~!@#$%^&*()_-+={[]}:;,.?/|\]+ quote
+    quote           -> "
+    number          -> [0-9]+
+    id              -> [a-zA-Z_]?[a-zA-Z0-9_]*
+    operator        -> + | - | * | /
+    """
+
+    # is_ptr, count_ast, i = check_ptr(tokens, i)
+    # # Check if identifier is present after var
+    # check_if(tokens[i].type, "id", "Expected id after var keyword", tokens[i].line_num)
+
+    # # Tokens that are not accepted after declaration of a variable
+    # invalid_tokens = [
+    #     "plus_equal",
+    #     "minus_equal",
+    #     "divide_equal",
+    #     "multiply_equal",
+    #     "plus",
+    #     "minus",
+    #     "divide",
+    #     "multiply",
+    #     "modulus",
+    #     "modulus_equal",
+    #     "equal",
+    #     "not_equal",
+    # ]
+    # # Check if variable is also initialized
+    # if i + 1 < len(tokens) and tokens[i + 1].type == "assignment":
+    #     # Store the index of identifier
+    #     id_idx = i
+
+    #     # Check if expression follows = in var statement
+    #     op_value, op_type, i, func_ret_type = expression(
+    #         tokens,
+    #         i + 2,
+    #         table,
+    #         "Required expression after assignment operator",
+    #         expect_paren=False,
+    #         func_ret_type=func_ret_type,
+    #     )
+
+    #     # Map datatype to appropriate datatype in C
+    #     prec_to_type = {
+    #         0: "string",
+    #         1: "string",
+    #         2: "char",
+    #         3: "int",
+    #         4: "float",
+    #         5: "double",
+    #     }
+
+    #     # Modify datatype of the identifier
+    #     table.symbol_table[tokens[id_idx].val][1] = prec_to_type[op_type]
+
+    #     if is_ptr:
+    #         return (
+    #             OpCode(
+    #                 "ptr_assign",
+    #                 table.symbol_table[tokens[id_idx].val][0]
+    #                 + "---"
+    #                 + op_value
+    #                 + "---"
+    #                 + str(count_ast),
+    #                 prec_to_type[op_type],
+    #             ),
+    #             i,
+    #             func_ret_type,
+    #         )
+    #     else:
+    #         # Return the opcode and i (the token after var statement)
+    #         return (
+    #             OpCode(
+    #                 "var_assign",
+    #                 table.symbol_table[tokens[id_idx].val][0] + "---" + op_value,
+    #                 prec_to_type[op_type],
+    #             ),
+    #             i,
+    #             func_ret_type,
+    #         )
+    # elif i + 1 < len(tokens) and tokens[i + 1].type in invalid_tokens:
+    #     error("Invalid Syntax for declaration", tokens[i].line_num)
+    # else:
+    #     # Get the value from symbol table by id
+    #     value, type, _ = table.get_by_id(tokens[i].val)
+
+    #     # If already declared then throw error
+    #     if type in [
+    #         "declared",
+    #         "int",
+    #         "char",
+    #         "float",
+    #         "double",
+    #         "string",
+    #         "char *",
+    #         "char*",
+    #     ]:
+    #         error("Variable %s already declared" % value, tokens[i].line_num)
+
+    #     # Set declared
+    #     table.symbol_table[tokens[i].val][1] = "declared"
+
+    #     # Return the opcode and i+1 (the token after var statement)
+    #     if is_ptr:
+    #         return OpCode("ptr_no_assign", value), i + 1, func_ret_type
+
+        return OpCode("array_no_assign", value), i + 1, func_ret_type
+
 
 def assign_statement(tokens, i, table, func_ret_type):
     """
@@ -1444,6 +1567,12 @@ def parse(tokens, table):
                 tokens, i + 1, table, func_ret_type
             )
             op_codes.append(var_opcode)
+        # If token if of type array then generate array opcode
+        elif tokens[i].type == "array":
+            array_opcode, i, func_ret_type = array_statement(
+                tokens, i + 1, table, func_ret_type
+            )
+            op_codes.append(array_opcode)
         # If token is of type id then generate assign opcode
         elif tokens[i].type == "id":
             # If '(' follows id then it is function calling else variable assignment
