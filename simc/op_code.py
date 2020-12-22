e74830c19723d321570cae14ef9f839550c7c933
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-12-22 22:43:42 +0530

    Fix comments in opcode class

--- simc/op_code.py
+++ simc/op_code.py
@@ -12,23 +12,15 @@ class OpCode:
         opcode           (string) = Type of opcode as string
         val              (string) = Value stored at opcode
         dtype (Optional) (string) = Datatype of opcode
-
-        Values
-        ======
-        type     (string) = Type of opcode as string
-        typedig  (int)    = Type of opcode as integer
-        val      (string) = Value stored at opcode
-        dtype    (string) = Datatype of opcode
         """
 
         self.type = opcode
-        self.typedig = self.opcode2dig(type)
         self.val = val
         self.dtype = dtype
 
     def __str__(self):
         """
-        Returns string representation of the object of this class
+        Returns string representation of OpCode
 
         Returns
         =======
@@ -36,35 +28,3 @@ class OpCode:
         """
 
         return "OpCode('%s', '%s', '%s')" % (self.type, self.val, self.dtype)
-
-    def opcode2dig(self, str_type):
-        """
-        Returns integer representation of opcode type
-
-        Params
-        ======
-        str_type (string) = String representation of opcode type
-
-        Returns
-        =======
-        int: The integer representation of the string opcode
-        """
-
-        dic = {
-            "print": 1,
-            "var_assign": 2,
-            "var_no_assign": 3,
-            "assign": 4,
-            "func_decl": 5,
-            "while": 6,
-            "if": 7,
-            "exit": 8,
-            "ptr_no_assign": 9,
-            "ptr_assign": 10,
-            "ptr_only_assign": 11,
-            "unary": 12,
-            "raw": 13,
-            "array_assign": 14,
-            "array_no_assign": 15,
-        }
-        return dic.get(str_type, 0)
