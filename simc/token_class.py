178a674835a07a525c2ae99113f3c96a01b6a032
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-12-22 22:42:21 +0530

    Fix comments in token class

--- simc/token_class.py
+++ simc/token_class.py
@@ -9,25 +9,19 @@ class Token:
 
         Params
         ======
-        type     (string) = type of token as string
-        val      (string) = value stored at token
-        line_num (int)    = line number
-
-        Values
-        ======
-        type     (string) = type of token as string
-        typedig  (int)    = type of token as integer
-        val      (string) = value stored at token
-        line_num (int)    = line number
+        type     (string) = Type of token as string
+        val      (string) = Value stored at token
+        line_num (int)    = Line number
         """
 
         self.type = type
-        self.typedig = self.token2dig(type)
         self.val = val
         self.line_num = line_num
 
     def __str__(self):
         """
+        String representation of a Token
+
         Returns
         =======
         string: The string representation of Token object, which can be used to print the tokens
@@ -37,6 +31,8 @@ class Token:
 
     def __eq__(self, other):
         """
+        Check for equality of tokens
+
         Returns
         =======
         bool: Whether a token object is equal to another or not
@@ -51,58 +47,3 @@ class Token:
             return True
 
         return False
-
-    def token2dig(self, str_type):
-        """
-        Params
-        ======
-        str_type (string) = String representation of token type
-
-        Returns
-        =======
-        int: The integer representation of the string token
-        """
-
-        dic = {
-            "number": 1,
-            "string": 2,
-            "print": 3,
-            "identifier": 4,
-            "left_paren": 5,
-            "right_paren": 6,
-            "var": 7,
-            "assignment": 8,
-            "plus": 9,
-            "minus": 10,
-            "multiply": 11,
-            "divide": 12,
-            "newline": 13,
-            "fun": 14,
-            "return": 15,
-            "equal": 16,
-            "not_equal": 17,
-            "greater_than": 18,
-            "less_than": 19,
-            "greater_than_equal": 20,
-            "less_than_equal": 21,
-            "while": 22,
-            "if": 23,
-            "modulus": 24,
-            "increment": 25,
-            "decrement": 26,
-            "plus_equal": 27,
-            "minus_equal": 28,
-            "multiply_equal": 29,
-            "divide_equal": 30,
-            "modulus_equal": 31,
-            "and": 32,
-            "or": 33,
-            "exit": 34,
-            "left_shift": 35,
-            "right_shift": 36,
-            "array": 37,
-            "left_bracket": 38,
-            "right_bracket": 39,
-        }
-
-        return dic.get(str_type, 0)
